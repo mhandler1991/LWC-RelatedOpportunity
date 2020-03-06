8e5cb22c31c76158dbd9dbaafdd8cc094a52cec9
Maxwell Handler <mhandler@corp.fivepointcapital.com>
2020-03-06 15:19:00 -0800

    base files

--- RelatedOpportunity/force-app/main/default/lwc/relatedOpportunityInformation/relatedOpportunityInformation.js
+++ RelatedOpportunity/force-app/main/default/lwc/relatedOpportunityInformation/relatedOpportunityInformation.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+// *********************
+// IMPORTS
+// *********************
+import { LightningElement, api, track, wire } from 'lwc';
+// Import Apex Class
+import getRelatedOpp from '@salesforce/apex/RelatedOpportunitySearch.getOpportunityInfo'
+// Import Record Information
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi'
 
-export default class RelatedOpportunityInformation extends LightningElement {}
+const FIELDS = [
+    'Opportunity.AccountId',
+]
+
+export default class RelatedOpportunityInformation extends LightningElement {
+
+    @api recordId; //Grab the Record Id
+
+    @wire(getRecord, {recordId: '$recordId', fields: FIELDS})
+    opportunity;
+
+    // Call RelatedOpportunitySearch Apex Class
+    // Grab Defined Related Opportunity Fields
+    @wire(getRelatedOpp, {recordId: '$recordId'}) opportunities;
+    
+
+}
